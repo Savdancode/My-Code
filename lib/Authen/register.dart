488ef95afd4fa516708f90b1e26fcc95291ee018
Soun Savdan <soun.savdnait@gmail.com>
2021-07-27 14:34:12 +0700

    Finish Code

--- lib/Authen/register.dart
+++ lib/Authen/register.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:cam_data/Authen/authen_Class.dart';
 import 'package:cam_data/Authen/signIn.dart';
 import 'package:cam_data/Net/setUser.dart';
 import 'package:cam_data/Screens/homesreen.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Register extends StatefulWidget {
   const Register({Key? key}) : super(key: key);
@@ -16,6 +19,8 @@ class _RegisterState extends State<Register> {
   TextEditingController _email = TextEditingController();
   TextEditingController _cpassword = TextEditingController();
   TextEditingController _password = TextEditingController();
+  File? _image;
+  final _picker = ImagePicker();
   final _formkey = GlobalKey<FormState>();
   bool isLoading = false;
   bool _isHiden = true;
@@ -28,7 +33,7 @@ class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text('Register'),
         centerTitle: true,
@@ -43,6 +48,7 @@ class _RegisterState extends State<Register> {
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
+                        _buildPickerImage,
                         TextFormField(
                           autofocus: false,
                           validator: (value) =>
@@ -58,7 +64,7 @@ class _RegisterState extends State<Register> {
                           ),
                         ),
                         SizedBox(
-                          height: 30,
+                          height: 20,
                         ),
                         TextFormField(
                           autofocus: false,
@@ -137,7 +143,7 @@ class _RegisterState extends State<Register> {
                                       password: _password.text.trim())
                                   .then((value) {
                                 if (value == "Account created") {
-                                  userSetup(_username.text);
+                                  userSetup(_username.text, _image);
                                   setState(() {
                                     isLoading = false;
                                   });
@@ -192,6 +198,89 @@ class _RegisterState extends State<Register> {
           : Center(
               child: CircularProgressIndicator(),
             ),
+    );
+  }
+
+  Future<void> _fromGallery() async {
+    final XFile? pickedFile =
+        await _picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _image = File(pickedFile!.path);
+    });
+  }
+
+  Future<void> _fromCamera() async {
+    final XFile? pickedFile =
+        await _picker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _image = File(pickedFile!.path);
+    });
+  }
+
+  void _showPicker(context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext bc) {
+        return SafeArea(
+          child: Container(
+            child: new Wrap(
+              children: <Widget>[
+                new ListTile(
+                    leading: new Icon(Icons.photo_library),
+                    title: new Text('Photo Library'),
+                    onTap: () {
+                      _fromGallery();
+                      Navigator.of(context).pop();
+                    }),
+                new ListTile(
+                  leading: new Icon(Icons.photo_camera),
+                  title: new Text('Camera'),
+                  onTap: () {
+                    _fromCamera();
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  get _buildPickerImage {
+    return Center(
+      child: GestureDetector(
+        onTap: () {
+          _showPicker(context);
+        },
+        child: CircleAvatar(
+          radius: 55,
+          backgroundColor: Colors.red,
+          child: _image != null
+              ? ClipRRect(
+                  borderRadius: BorderRadius.circular(50),
+                  child: Image.file(
+                    _image!,
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  ),
+                )
+              : Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  width: 100,
+                  height: 100,
+                  child: Icon(
+                    Icons.camera_alt,
+                    color: Colors.grey[800],
+                  ),
+                ),
+        ),
+      ),
     );
   }
 }
--- lib/Net/setUser.dart
+++ lib/Net/setUser.dart
@@ -1,10 +1,28 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
-Future<void> userSetup(String displayName) async {
+Future<String> userSetup(String displayName, File? _image) async {
   CollectionReference users = FirebaseFirestore.instance.collection('Users');
   FirebaseAuth auth = FirebaseAuth.instance;
+  await firebase_storage.FirebaseStorage.instance
+      .ref('upload/${_image!.path.split("/").last}')
+      .putFile(_image);
+  String downloadURL = await firebase_storage.FirebaseStorage.instance
+      .ref('upload/${_image.path.split("/").last}')
+      .getDownloadURL();
   String uid = auth.currentUser!.uid.toString();
-  users.add({'displayName': displayName, 'uid': uid});
-  return;
+  users
+      .add(
+        {
+          'displayName': displayName,
+          'uid': uid,
+          'url': downloadURL,
+        },
+      )
+      .then((value) => 'Data has been submited!')
+      .catchError((error) => 'Failed to add user: $error');
+  return '';
 }
