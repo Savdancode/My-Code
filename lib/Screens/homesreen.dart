5236f066110da419e4584d85f5833f1c01f7fa65
Soun Savdan <soun.savdnait@gmail.com>
2021-07-28 23:21:37 +0700

    done

--- lib/Screens/homesreen.dart
+++ lib/Screens/homesreen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,14 +10,36 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String uid = FirebaseAuth.instance.currentUser!.uid;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          color: Colors.white,
-          child: Column(),
-        ),
+      body: FutureBuilder<QuerySnapshot>(
+        future: FirebaseFirestore.instance.collection('Users').get(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final List<DocumentSnapshot> documents = snapshot.data!.docs;
+            return ListView(
+              children: documents
+                  .map(
+                    (doc) => Card(
+                      child: ListTile(
+                        title: Text(doc['displayName']),
+                      ),
+                    ),
+                  )
+                  .toList(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('error'),
+            );
+          } else {
+            return Center(
+              child: Text('Loading...'),
+            );
+          }
+        },
       ),
     );
   }
